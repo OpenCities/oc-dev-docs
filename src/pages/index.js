d8bed5f59fdb248c663a1a06f172799a2d813537
Michelle Hertzfeld <mhertzfeld@gmail.com>
2020-09-06 11:04:40 +1000

    Rename heading on homepage. Jump start.

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import styles from './styles.module.css';
 
 const features = [
   {
-    title: <>Easy to Use</>,
+    title: <>What about renaming this?</>,
     imageUrl: 'img/undraw_docusaurus_mountain.svg',
     description: (
       <>
